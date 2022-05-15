2f89ddb95aaf8a0a3dcc4fbca8b60371696e8030
Nino Annighoefer <nino@ninoan.com>
2022-05-15 18:38:37 +0100

    Some GUI stuff

--- projects/gogui/go.mod
+++ projects/gogui/go.mod
@@ -7,4 +7,6 @@ require github.com/diamondburned/gotk4/pkg v0.0.0-20220418035147-8a785583d00a
 require (
 	go4.org/unsafe/assume-no-moving-gc v0.0.0-20211027215541-db492cf91b37 // indirect
 	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/sys v0.0.0-20211019181941-9d821ace8654 // indirect
+	golang.org/x/tools v0.1.10 // indirect
 )
--- projects/gogui/gui.go
+++ projects/gogui/gui.go
@@ -1,22 +1,11 @@
 package main
 
 import (
-	"github.com/diamondburned/gotk4/pkg/gdk/v4"
 	"github.com/diamondburned/gotk4/pkg/gtk/v4"
 	"log"
 	"os"
-)
-
-var todos []string = []string{"One", "Two"}
-var todosBox *gtk.Box
-var globalWin *gtk.Window
-
-const (
-	MOD_NOTHING = uint(0)
-	MOD_SHIFT   = uint(gdk.SHIFT_MASK)
-	MOD_CTRL    = uint(gdk.CONTROL_MASK)
-	MOD_OPTION  = uint(gdk.MOD1_MASK)
-	MOD_COMMAND = uint(gdk.MOD2_MASK)
+	"strconv"
+	"strings"
 )
 
 // KeyVal values
@@ -24,10 +13,8 @@ const (
 	KEY_RETURN = 65293
 )
 
-func isKeyCombo(event *gdk.Event, modMasks uint, keyVal uint) bool {
-	keyEvent := &gdk.EventKey{Event: event}
-	state := keyEvent.State() & (MOD_NOTHING | MOD_CTRL | MOD_COMMAND | MOD_OPTION | MOD_SHIFT)
-	return (state == modMasks) && keyEvent.KeyVal() == keyVal
+func formatNumber(num int) string {
+	return strings.ReplaceAll(strconv.Itoa(num), "-", "–")
 }
 
 func maybeFail(msg string, err error) {
@@ -36,125 +23,79 @@ func maybeFail(msg string, err error) {
 	}
 }
 
-func todoItem(text string) *gtk.Box {
-	label, labelErr := gtk.NewLabel(text)
-	maybeFail("Unable to create label: ", labelErr)
-	button, buttonErr := gtk.NewButtonWithLabel("×") // This is very bad
-	maybeFail("Unable to create button: ", buttonErr)
+type CounterComponent struct {
+	Widget *gtk.Box
+	Value  int
 
-	layout, err := gtk.NewBox(gtk.OrientationHorizontal, 8)
-	maybeFail("Unable to create layout for todo item: ", err)
-
-	layout.Add(button)
-	layout.Add(label)
-	return layout
+	display *gtk.Label
 }
 
-func initTodoList() {
-	layout, boxErr := gtk.NewBox(gtk.OrientationVertical, 8)
-	if boxErr != nil {
-		log.Fatal("Unable to create stack", boxErr)
-	}
-
-	for _, t := range todos {
-		label := todoItem(t)
-		layout.Add(label)
-	}
-
-	todosBox = layout
+func (counter *CounterComponent) Increment() {
+	counter.Value += 1
+	counter.display.SetText(formatNumber(counter.Value))
 }
 
-func addTodo(text string) {
-	log.Print("Adding TODO", text)
-	widget := todoItem(text)
-	todosBox.Add(widget)
-	todosBox.CheckResize()
-	log.Print(todosBox.GetChildren().Length())
-	todosBox.GetChildren().Foreach(func(child interface{}) {
-		log.Print("Child: ", child)
-	})
-	todosBox.ShowAll()
+func (counter *CounterComponent) Decrement() {
+	counter.Value -= 1
+	counter.display.SetText(formatNumber(counter.Value))
 }
 
-var newTodoForm *gtk.Box
+func NewCounterComponent(initialValue int) CounterComponent {
+	box := gtk.NewBox(gtk.OrientationVertical, 4)
+	box.SetMarginTop(8)
+	box.SetMarginBottom(8)
+	box.SetMarginStart(8)
+	box.SetMarginEnd(8)
+	box.SetHExpand(true)
+	box.SetVExpand(true)
 
-func initNewTodoForm() {
-	layout, boxErr := gtk.NewBox(gtk.ORIENTATION_HORIZONTAL, 8)
-	if boxErr != nil {
-		log.Fatal("Unable to create box for new todo form:", boxErr)
+	display := gtk.NewLabel(formatNumber(initialValue))
+	display.SetHExpand(true)
+	display.SetVExpand(true)
+	display.SetVAlign(gtk.AlignCenter)
+
+	buttonBox := gtk.NewBox(gtk.OrientationHorizontal, 4)
+	buttonBox.SetHExpand(true)
+	buttonBox.SetVExpand(false)
+	buttonBox.SetHAlign(gtk.AlignCenter)
+
+	plusButton := gtk.NewButtonWithLabel("+")
+	plusButton.SetSizeRequest(16, 16)
+	minusButton := gtk.NewButtonWithLabel("–")
+	buttonBox.Append(plusButton)
+	buttonBox.Append(minusButton)
+
+	box.Append(display)
+	box.Append(buttonBox)
+
+	component := CounterComponent{
+		Value:   initialValue,
+		Widget:  box,
+		display: display,
 	}
 
-	textField, textFieldErr := gtk.EntryNew()
-	if textFieldErr != nil {
-		log.Fatal("Unable to create text view:", textFieldErr)
-	}
-	layout.Add(textField)
+	plusButton.ConnectClicked(func() { component.Increment() })
+	minusButton.ConnectClicked(func() { component.Decrement() })
 
-	addButton, buttonErr := gtk.NewButtonWithLabel("Add")
-	if buttonErr != nil {
-		log.Fatal("Unable to add button:", buttonErr)
-	}
-
-	doAddTodo := func() {
-		text, _ := textField.GetText()
-		addTodo(text)
-		textField.SetText("")
-	}
-
-	addButton.Connect("clicked", doAddTodo)
-	layout.Add(addButton)
-
-	textField.Connect("key-press-event", func(field *gtk.Entry, event *gdk.Event) {
-		if isKeyCombo(event, MOD_NOTHING, KEY_RETURN) {
-			doAddTodo()
-		}
-	})
-
-	newTodoForm = layout
-}
-
-func mainContainer() *gtk.Box {
-	layout, boxErr := gtk.NewBox(gtk.OrientationVertical, 8)
-	if boxErr != nil {
-		log.Fatal("Unable to create main container:", boxErr)
-	}
-
-	initTodoList()
-	layout.Add(todosBox)
-
-	initNewTodoForm()
-	layout.Add(newTodoForm)
-
-	layout.SetBorderWidth(8)
-	return layout
-}
-
-func setUpOpeningFiles(app *gtk.Application) {
-	log.Print(app)
+	return component
 }
 
 func main() {
-	app := gtk.NewApplication("com.ninoan.gtk4-app", 0)
+	app := gtk.NewApplication("com.ninoan.todo-test", 0)
+	app.ConnectActivate(func() { activate(app) })
 
-	win, err := gtk.NewWindow()
-	if err != nil {
-		log.Fatal("Unable to create window:", err)
+	statusCode := app.Run(os.Args)
+	if statusCode > 0 {
+		os.Exit(statusCode)
 	}
-	win.SetTitle("TodoMVC")
-	win.Connect("destroy", func() {
-		gtk.MainQuit()
-	})
-	win.Connect("key-press-event", func(win *gtk.Window, event *gdk.Event) {
-		if isKeyCombo(event, MOD_COMMAND, 'w') {
-			win.Close()
-		}
-	})
+}
 
-	globalWin = win
-	mainLayout := mainContainer()
-	win.Add(mainLayout)
+func activate(app *gtk.Application) {
+	box := gtk.NewBox(gtk.OrientationVertical, 4)
+	box.Append(NewCounterComponent(2).Widget)
 
-	// Recursively show all widgets contained in this window.
-	win.ShowAll()
-	gtk.Main()
+	mainWindow := gtk.NewApplicationWindow(app)
+	mainWindow.SetTitle("Counter App")
+	mainWindow.SetChild(box)
+	mainWindow.Show()
 }
