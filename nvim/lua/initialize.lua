2b0f59b3d3f8baecdbfaefef9f3f6aa08b1f5bfd
Nino Annighoefer <nino@ninoan.com>
2022-10-10 10:41:30 +0100

    Exclude .git from fd

--- nvim/lua/initialize.lua
+++ nvim/lua/initialize.lua
@@ -62,7 +62,7 @@ vim.cmd("command! A normal ddnVnd")
 vim.cmd("command! B normal Vndndd")
 
 -- Variables
-vim.env.FZF_DEFAULT_COMMAND = "fd --type f --hidden"
+vim.env.FZF_DEFAULT_COMMAND = "fd --type f --hidden --exclude .git"
 
 -- LSP
 local on_attach = function(_, _)
