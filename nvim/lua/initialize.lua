e98aaf188f7c0e8d9c183f4f3da08efbadc5526f
Nino Annighoefer <nino@ninoan.com>
2022-11-21 09:51:00 +0000

    Maybe some vim stuff idk

--- nvim/lua/initialize.lua
+++ nvim/lua/initialize.lua
@@ -39,7 +39,8 @@ vim.opt.autoread = true
 vim.opt.diffopt = vim.opt.diffopt + "iwhite"
 -- vim.opt.signcolumn = "yes"
 vim.opt.list = true
-vim.opt.number = false
+vim.opt.number = true
+vim.opt.numberwidth = 1
 vim.opt.listchars = "tab:→ ,nbsp:␣,trail:⌁,extends:→,precedes:←"
 
 vim.opt.rulerformat = '♥︎ %l/%L %P %c'
