cad79ec5cf00dd5eebfdadf1186e9d5b8fc5ee08
Nino Annighoefer <nino@ninoan.com>
2023-02-01 19:38:04 +0000

    Stuff

--- nvim/lua/initialize.lua
+++ nvim/lua/initialize.lua
@@ -1,36 +1,36 @@
 local utils = require("utils")
 
 -- Colors
-local use_light_bg = false
+local use_light_bg = true
 vim.opt.termguicolors = true
 if use_light_bg then
-  -- utils.color("thinglight")
-  utils.color("morning")
+  utils.color("thinglight")
+  -- utils.color("morning")
 else
   vim.o.background = "dark"
 
-  utils.color("thing")
+  -- utils.color("thing")
   -- utils.color("plaindark")
   -- utils.color("desert")
 
-  -- require("gruvbox").setup({
-  --   undercurl = true,
-  --   underline = true,
-  --   bold = true,
-  --   italic = true,
-  --   strikethrough = true,
-  --   invert_selection = false,
-  --   invert_signs = false,
-  --   invert_tabline = false,
-  --   invert_intend_guides = false,
-  --   inverse = true, -- invert background for search, diffs, statuslines and errors
-  --   contrast = "hard", -- can be "hard", "soft" or empty string
-  --   palette_overrides = {},
-  --   overrides = {},
-  --   dim_inactive = false,
-  --   transparent_mode = false,
-  -- })
-  -- utils.color("gruvbox")
+  require("gruvbox").setup({
+    undercurl = true,
+    underline = true,
+    bold = true,
+    italic = true,
+    strikethrough = true,
+    invert_selection = false,
+    invert_signs = false,
+    invert_tabline = false,
+    invert_intend_guides = false,
+    inverse = true, -- invert background for search, diffs, statuslines and errors
+    contrast = "hard", -- can be "hard", "soft" or empty string
+    palette_overrides = {},
+    overrides = {},
+    dim_inactive = false,
+    transparent_mode = false,
+  })
+  utils.color("gruvbox")
 end
 
 -- Defaults
