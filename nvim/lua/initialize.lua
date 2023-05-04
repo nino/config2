c7fb795945373ae2da145b36fad427890068514e
Nino Annighoefer <nino@ninoan.com>
2023-05-04 18:12:58 +0100

    Just use global eslint

--- nvim/lua/initialize.lua
+++ nvim/lua/initialize.lua
@@ -230,7 +230,7 @@ vim.cmd([[
   nnoremap <silent> K :lua vim.lsp.buf.hover()<CR>
   nnoremap <silent> gd :lua vim.lsp.buf.definition()<CR>
   nnoremap <silent> _ :lua format_file()<CR>
-  nnoremap <silent> <leader>- :!yarn exec eslint --fix %<CR>
+  nnoremap <silent> <leader>- :!eslint --fix %<CR>
   nnoremap <silent> ,n :lua vim.diagnostic.goto_next()<CR>
   nnoremap <silent> <c-n> :lua vim.diagnostic.goto_next()<CR>
   nnoremap <silent> ,a :lua vim.lsp.buf.code_action()<CR>
