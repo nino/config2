c6b1a5eed37e1bdf8c669f4413e201dfba39a248
Nino Annighoefer <nino@ninoan.com>
2023-06-02 10:09:43 +0100

    Neovim has a builtin colorscheme that isn't broken???

--- nvim/after/plugin/everything.lua
+++ nvim/after/plugin/everything.lua
@@ -77,21 +77,4 @@ require("catppuccin").setup({
     color_overrides = {},
     custom_highlights = {},
 })
-require("gruvbox").setup({
-    undercurl = false,
-    underline = false,
-    bold = true,
-    italic = {},
-    strikethrough = true,
-    invert_selection = false,
-    invert_signs = false,
-    invert_tabline = false,
-    invert_intend_guides = false,
-    inverse = true,    -- invert background for search, diffs, statuslines and errors
-    contrast = "hard", -- can be "hard", "soft" or empty string
-    palette_overrides = {},
-    overrides = {},
-    dim_inactive = false,
-    transparent_mode = false,
-})
-vim.cmd.colorscheme('catppuccin')
+vim.cmd.colorscheme('lunaperche')
