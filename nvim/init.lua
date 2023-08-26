18c71a51b6a086acc76c3bf23e8eff4ae1779843
Nino Annighoefer <nino@ninoan.com>
2023-08-26 10:10:11 +0100

    Dark

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,7 +13,7 @@ vim.opt.signcolumn = "yes"
 vim.opt.scrolloff = 4
 vim.opt.smartindent = true
 vim.opt.autoread = true
-vim.opt.bg = 'light'
+vim.opt.bg = 'dark'
 
 vim.opt.wildignorecase = true
 vim.opt.ignorecase = true
