d7890a6cb5357a25886dd554c618e9cfda11822a
Nino Annighoefer <nino@ninoan.com>
2023-09-10 20:33:49 +0100

    Use custom installation of Gleam

--- nvim/after/plugin/everything.lua
+++ nvim/after/plugin/everything.lua
@@ -15,7 +15,7 @@ lsp.ensure_installed({
     'eslint',
     'clangd',
     'svelte',
-    'gleam',
+    -- 'gleam',
     'pylsp'
 })
 
@@ -67,15 +67,10 @@ lsp.configure('lua_ls', {
     }
 })
 
-
-function gleam_test_lsp()
-  return {
-    name = 'gleam-test-lsp',
-    cmd = { "/Users/Nino/code-friends/gleam/target/debug/gleam", "lsp" },
-    filetypes = "gleam",
-    single_file_support = true,
-  }
-end
+lsp.configure('gleam', {
+    -- set custom executable path
+    cmd = { "/Users/nino/.cargo/bin/gleam", "lsp" },
+})
 
 lsp.setup()
 
