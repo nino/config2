be5209c146fa97f8748bccec5afd76e9e173a010
Nino Annighoefer <nino@ninoan.com>
2024-01-03 16:25:26 +0000

    Edit DAP key maps

--- nvim/after/plugin/dap.lua
+++ nvim/after/plugin/dap.lua
@@ -1,6 +1,14 @@
-vim.keymap.set("n", "<leader>dc", "<cmd>lua require('dap').continue()<CR>", { noremap = true, silent = true })
-vim.keymap.set("n", "<leader>db", "<cmd>lua require('dap').toggle_breakpoint()<CR>", { noremap = true, silent = true })
+local dap = require("dap")
+
+vim.keymap.set("n", "<leader>db", function() dap.toggle_breakpoint() end, { noremap = true, silent = true })
+vim.keymap.set("n", "<m-1>", function() dap.step_into() end, { noremap = true, silent = true })
+vim.keymap.set("n", "<m-2>", function() dap.step_over() end, { noremap = true, silent = true })
+vim.keymap.set("n", "<m-3>", function() dap.step_out() end, { noremap = true, silent = true })
+vim.keymap.set("n", "<m-4>", function() dap.continue() end, { noremap = true, silent = true })
 
 require("dap-go").setup()
 require("dap-python").setup()
-require("dapui").setup()
+local dapui = require("dapui")
+dapui.setup()
+
+vim.api.nvim_create_user_command("DUI", function() dapui.toggle() end, {})
