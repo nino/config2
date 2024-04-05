b73f46f5d65df19bdc2bba4473b4c76685a6dd6f
Nino Annighoefer <nino@ninoan.com>
2024-04-05 09:49:19 +0100

    Colors

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,6 +1,13 @@
+vim.cmd [[
+  hi Comment cterm=italic gui=italic
+]]
+
 if vim.o.bg == 'light' then
   vim.cmd [[
-    hi Comment cterm=italic gui=italic
     hi DiffText cterm=bold gui=bold ctermbg=225 guibg=LightRed
+  ]]
+else
+  vim.cmd [[
+    hi DiffText cterm=bold gui=bold ctermbg=225 guibg=DarkRed
   ]]
 end
