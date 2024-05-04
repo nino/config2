cb3afa8fe7a5da84153a71164bd68fc33c9ce4a7
Nino Annighoefer <nino@ninoan.com>
2024-05-04 20:35:19 +0100

    Lunaperche

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -1,16 +1,14 @@
+vim.cmd [[ colorscheme lunaperche ]]
 vim.cmd [[
-  colorscheme gruvbox
+  hi Comment cterm=italic gui=italic
 ]]
--- vim.cmd [[
---   hi Comment cterm=italic gui=italic
--- ]]
 
--- if vim.o.bg == 'light' then
---   vim.cmd [[
---     hi DiffText cterm=bold gui=bold ctermbg=225 guibg=LightRed
---   ]]
--- else
---   vim.cmd [[
---     hi DiffText cterm=bold gui=bold ctermbg=225 guibg=DarkRed
---   ]]
--- end
+if vim.o.bg == 'light' then
+  vim.cmd [[
+    hi DiffText cterm=bold gui=bold ctermbg=225 guibg=LightRed
+  ]]
+else
+  vim.cmd [[
+    hi DiffText cterm=bold gui=bold ctermbg=225 guibg=DarkRed
+  ]]
+end
