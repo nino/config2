37cd4688ce9916ca5cc54698f612c6f574df9d93
Nino Annighoefer <nino.annighoefer@kennek.io>
2024-08-15 09:22:06 +0100

    Undercurl

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -2,11 +2,13 @@
 vim.o.bg='light'
 vim.cmd [[
   hi Comment cterm=italic gui=italic
+  hi DiagnosticUnderlineError cterm=underline gui=undercurl
 ]]
 
 if vim.o.bg == 'light' then
   vim.cmd [[
     hi DiffText cterm=bold gui=bold ctermbg=225 guibg=LightRed
+
   ]]
 else
   vim.cmd [[
