e4b34f801e8d18ce6a7475b5d11ee0f42bc0c08e
Nino Annighoefer <nino@ninoan.com>
2025-04-02 13:07:19 +0100

    shrug

--- fish/config.fish
+++ fish/config.fish
@@ -80,9 +80,9 @@ if [ -e /usr/local/bin/nvim ];
   export EDITOR=/usr/local/bin/nvim
 end
 
-export REACT_EDITOR='zed'
-export EDITOR='zed'
-export VISUAL='zed'
+export REACT_EDITOR='cursor'
+export EDITOR='cursor'
+export VISUAL='cursor'
 export CMAKE_EXPORT_COMPILE_COMMANDS=true
 export PACKERPATH=$HOME/.local/share/nvim/site/pack/packer/start
 export HOMEBREW_AUTO_UPDATE_SECS=600
