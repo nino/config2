37ad0d795b7333022b905ced45bfc7aab10c321a
Nino Annighoefer <nino@ninoan.com>
2025-08-31 18:11:20 +0100

    Colours

--- nvim/after/plugin/colors.lua
+++ nvim/after/plugin/colors.lua
@@ -4,12 +4,12 @@ function CheckAppearance()
   local theme = vim.fn.system('defaults read -g AppleInterfaceStyle'):gsub('\n', '')
   if theme == 'Dark' then
     vim.o.background = 'dark'
-    vim.cmd('colorscheme modus')
+    -- vim.cmd('colorscheme modus')
     vim.cmd [[
       " hi Normal guibg=clear
-      hi TabLineSel guibg=#999999
-      hi DiagnosticUnderlineError gui=none
-      hi DiagnosticUnderlineWarn gui=none
+      " hi TabLineSel guibg=#999999
+      " hi DiagnosticUnderlineError gui=none
+      " hi DiagnosticUnderlineWarn gui=none
       " hi DiffText cterm=bold gui=bold ctermbg=225 guibg=DarkRed
       " hi RenderMarkdown_bgtofg_RenderMarkdownCode guifg=#1e1e1e
       " hi NonText guifg=#333333
@@ -21,45 +21,30 @@ function CheckAppearance()
     ]]
   else
     vim.o.background = 'light'
-    vim.cmd('colorscheme modus')
+    -- vim.cmd('colorscheme modus')
     vim.cmd [[
-      hi Normal guibg=clear
-      hi NormalNC guibg=clear
-      hi Pmenu guibg=#eeeeee
-      hi DiagnosticError guifg=#660000
-      hi DiagnosticFloatingHint guifg=#111111
-      hi TabLineSel guibg=#EEEEEE
-      " hi AvanteTitle guifg=#1e222a guibg=#98c379
-      " hi AvanteReversedTitle guifg=#98c379
-      " hi AvanteSubtitle guifg=#1e222a guibg=#56b6c2
-      " hi AvanteReversedSubtitle guifg=#56b6c2
-      " hi AvanteThirdTitle guifg=#abb2bf guibg=#353b45
-      " hi AvanteReversedThirdTitle guifg=#353b45
-      hi AvanteConflictCurrent cterm=bold gui=bold guibg=#66ccff
-      hi AvanteToBeDeletedWOStrikethrough cterm=bold gui=bold guibg=#66ccff
-      hi AvanteConflictIncoming cterm=bold gui=bold guibg=#33ffaa
-      " hi AvanteConflictCurrentLabel guibg=#6f393e
-      hi AvanteConflictIncomingLabel guibg=#3f5c6b
-      " hi def link AvantePopupHint NormalFloat
-      " hi def link AvanteInlineHint Keyword
-      " hi clear AvantePromptInput
-      " hi def link AvantePromptInputBorder NormalFloat
+      " hi Normal guibg=clear
+      " hi NormalNC guibg=clear
+      " hi Pmenu guibg=#eeeeee
+      " hi DiagnosticError guifg=#660000
+      " hi DiagnosticFloatingHint guifg=#111111
+      " hi TabLineSel guibg=#EEEEEE
 
-      hi DiffAdd guibg=#DDFFDD
-      hi DiffChange guibg=#DDFFDD
-      hi DiffText cterm=bold gui=bold ctermbg=225 guibg=#BBEEBB
-      hi DiffDelete guifg=#FFDDDD guibg=#FFEEEE
-      hi diffRemoved guibg=#FFEEEE
+      " hi DiffAdd guibg=#DDFFDD
+      " hi DiffChange guibg=#DDFFDD
+      " hi DiffText cterm=bold gui=bold ctermbg=225 guibg=#BBEEBB
+      " hi DiffDelete guifg=#FFDDDD guibg=#FFEEEE
+      " hi diffRemoved guibg=#FFEEEE
 
       " hi RenderMarkdown_bgtofg_RenderMarkdownCode guifg=#f2f2f2
       " hi NonText guifg=#eeeeee
-      hi CursorLine guibg=#fafafd
-      hi CursorColumn guibg=#eeeeff
-      hi ColorColumn guibg=#eeeeff
+      " hi CursorLine guibg=#fafafd
+      " hi CursorColumn guibg=#eeeeff
+      " hi ColorColumn guibg=#eeeeff
       " hi htmlBold gui=bold
-      hi DiagnosticUnderlineError gui=underline guisp=#f2AAAA
-      hi DiagnosticUnderlineWarn gui=underline guisp=#e2e222
-      hi DiagnosticUnderlineHint gui=underline guisp=#cccccc
+      " hi DiagnosticUnderlineError gui=underline guisp=#f2AAAA
+      " hi DiagnosticUnderlineWarn gui=underline guisp=#e2e222
+      " hi DiagnosticUnderlineHint gui=underline guisp=#cccccc
       " hi SignColumn guibg=#eeeeee
       " hi LineNr guifg=#777777
       " hi Comment cterm=italic gui=italic guifg=#885588
